8424ca0eeca6b021af1a6405e34321a39ebc87eb
Darshan Satya <darshans1989@gmail.com>
2019-11-16 16:42:50 -0500

    hash implementation - sha256

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,7 @@
-import functools
+from functools import reduce
+import hashlib as hl
+import json
+
 # Initializing blockchain list
 MINING_REWARD = 10
 
@@ -12,20 +15,20 @@ participants = {'Max'}
 
 
 def hash_block(block):
-    return '-'.join([str(block[key]) for key in block])
-
+    # return '-'.join([str(block[key]) for key in block])
+    return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transaction']
                   if tx['sender'] == participant] for block in blockchain]
 
-    amount_sent = functools.reduce(
+    amount_sent = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
     tx_recipient = [[tx['amount'] for tx in block['transaction']
                      if tx['recipient'] == participant] for block in blockchain]
 
-    amount_recieved = functools.reduce(
+    amount_recieved = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
 
     print('amount_recieved', amount_recieved)
